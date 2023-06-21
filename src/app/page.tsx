720739278e6925f4c6e77e0977edafdae235ae21
serzh <serzhan181@outlook.com>
2023-06-21 18:11:41 +0600

    add first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <p className="text-primary-foreground">Hello</p>
+    <main className="container">
+      <div className="flex gap-6 py-5">
+        <Link href="/" className="font-semibold">
+          Music
+        </Link>
+        <Link href="/" className="font-semibold text-muted-foreground">
+          Podcast
+        </Link>
+        <Link href="/" className="font-semibold text-muted-foreground">
+          Live
+        </Link>
+        <Link href="/" className="font-semibold text-muted-foreground">
+          Radio
+        </Link>
+      </div>
     </main>
   );
 }
